19018b887b428bb5db147ccb2d2f2b7408c5ec12
AlbertoGongora <gongorabada@gmail.com>
2024-07-03 17:39:40 +0200

    Primer commit con la navegacion de paginas dentro del main

---
+++ src/pages/Home.ts
@@ -0,0 +1,7 @@
+export function renderHome(): string {
+    return `
+      <h2>Home</h2>
+      <p>Página de inicio.</p>
+    `;
+  }
+  
---
+++ src/pages/Playlists.ts
@@ -0,0 +1,7 @@
+export function renderPlaylists(): string {
+    return `
+      <h2>Playlists</h2>
+      <p>Tus playlists.</p>
+    `;
+  }
+  
